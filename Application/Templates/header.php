bfe5706798eaab0b0488facc38cafba56388a50a
mnyerczan <mnyerczan@outlook.hu>
2020-04-07 10:01:23 +0200

    200407

--- Application/Templates/header.php
+++ Application/Templates/header.php
@@ -2,6 +2,7 @@
     <span>&#931;</span>
     <a href="<?= APPROOT ?>/"       class="<?=@$home?>">&#919;ome</a>
     <a href="<?= APPROOT ?>/about"  class="<?=@$about?> about">&#923;bout</a>   
-    <a href="<?= APPROOT ?>/module" class="<?=@$modlue?>">Module</a>   
     <!-- Ide jönnek a feladatspecifikus menüpontok -->
+        <!-- Példa modul menüpont -->
+    <a href="<?= APPROOT ?>/module" class="<?=@$modlue?>">Module</a>       
 </header>
